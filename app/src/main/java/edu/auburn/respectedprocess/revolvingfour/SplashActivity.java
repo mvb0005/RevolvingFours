c376555d2e0bf4c43509a7bea4288f5fdecc880a
SweatyBurger <bmw626262@live.com>
2018-04-28 23:16:29 -0500

    Add splash screen

---
+++ app/src/main/java/edu/auburn/respectedprocess/revolvingfour/SplashActivity.java
@@ -0,0 +1,26 @@
+package edu.auburn.respectedprocess.revolvingfour;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 5000);
+    }
+}
